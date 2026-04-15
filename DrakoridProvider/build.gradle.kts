6d1cf8d7f78418bad85e917c84781b40da55b4eb
duro92 <pendisu647@gmail.com>
2026-04-15 13:48:50 +0700

    Fix

---
+++ DrakoridProvider/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "Drakor.id"
+    language = "id"
+    authors = listOf("Codex")
+
+    status = 1
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://drakorid.co&size=%size%"
+}
