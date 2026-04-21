f9f0ae9470f1f74835f77f3b31e23f7fb5606651
duro92 <pendisu647@gmail.com>
2026-04-21 14:45:33 +0700

    Fix

--- Sflix/build.gradle.kts
+++ Sflix/build.gradle.kts
@@ -13,6 +13,6 @@ cloudstream {
     )
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://sflix.film&size=%size%"
 
-    requiresResources = true
+    requiresResources = false
     isCrossPlatform = false
 }
